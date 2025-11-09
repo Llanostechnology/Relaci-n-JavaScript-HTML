84b001ac51baf95192154b0e9bddf550b6fde19a
llanostecnology <llamostechnology@gamil.com>
2025-11-09 16:10:10 -0500

    como acceder a nodos

---
+++ JS/objetoDocumento.js
@@ -0,0 +1,3 @@
+// cada vez vez que abrimos un sitio web sucede algo muy importante para un desarrollador web: se crea un DOM. DOM es una estructura que crea nodos, es decir, que crea un árbol y crea una relación entre los nodos, tenemos nodos padres y nodos hijos.
+
+document.write("texto");
---
+++ JS/window.js
@@ -0,0 +1,27 @@
+//cuando hacemos clic en el navegador para abrirlo se crea un objeto window, este objeto es el navegador que estamos utilizando, como caulquier objeto contamos con atributos y métodos que podemos modificar 
+// el objeto window es al que accedemos aunque wn muchos casos lo vamos a tener que hacer desde la propiedad window, que es la forna en que accederemos al objeto
+// el método més famoso del objeto window es alert(),hasta este apartado del libro siempre queremos utilizar el método alert hacemos lo siguiente
+
+// alert("colocamos el texto");
+
+//la forma estricta de escribirla seria:
+
+window.alert("Colocamos el texto");
+
+//Propiedades y metodos de window
+
+// vamos a describir las propiedades y los metodos de windwow
+
+// Propieda/metodo                            // Qué hace
+
+//alert ()                                   despliega una caja de texto con la información que le colocamos 
+
+// confirm                                   despliega una caja de texto con 2 opciones: SI y NO
+
+//history                                    lo utilizamos para poder navegar por el historial de la ventana
+
+//location                                   podemos acceder a la información de la url
+
+//setinterval                                nos permite llamar funciones por una frecuencia que definimos
+
+//setTimeout                                 ejecutamos una función que indiquemos al transcurrir los milisegundos que le especifiquemos
