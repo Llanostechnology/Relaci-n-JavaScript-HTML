1602e40b9814073f5d3909fa87bb1c6125fe6873
llanostecnology <llamostechnology@gamil.com>
2025-11-10 14:38:02 -0500

    atributos JavaScript

---
+++ JS/atributosJavaScript.js
@@ -0,0 +1,6 @@
+// Al entender que todo documento HTML es un árbol de nodos, ya sabemos que podemos manipularlos, craerlos o destruirlos. También podemos acceder a los atributos de los nodos y modificarlos.
+// En el siguiente código anterior lo primero que hacemos es acceder al elemento de forma directa seleccionándolo por ID, después entramos a su estilo y especificamente a la propiedad de margen, cambiando el valor que teníamos en un principio en 0px a 10px
+document.getElementById("texto").styles.margin = "10px";
+
+
+
--- JS/eliminarNodos.js
+++ JS/eliminarNodos.js
@@ -1,1 +1,10 @@
 // Para eliminar nodos solamente tenemos que utilizar la función removeChild();
+
+// ahora vamos a eliminar desde JavaScript el nodo del parrafo
+
+var parrafo = document.getElementById("texto")
+
+parrafo.parentNode.removeChild(parrafo);
+
+// una regla para eliminar un nodo es hacerlo desde el nodo padre, por eso utilizamos parentNode.
+
